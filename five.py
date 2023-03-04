1e61fd99ff9ced782d06b9a5e82aad8b39d5698f
vraavana90 <126935521+vraavana90@users.noreply.github.com>
2023-03-04 10:36:06 +0530

    Add files via upload

---
+++ five.py
@@ -0,0 +1,10 @@
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+c=int(input("enter c value"))      
+if(a>b)and(a>c):
+    print("a is big")
+elif(b>a)and(b>c):
+    print("b is big")
+else:
+     print("c is big")
+
